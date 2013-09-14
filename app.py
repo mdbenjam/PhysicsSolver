1a8ff15d05a4cdff89824341233d51d72c850ef0
Ceasar Bautista <cbautista2010@gmail.com>
2013-09-14 09:45:50 -0700

    Make files play nicely with Flask

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
+def double(x):
+    return x * 2
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        return str(request.form)
+        return str(double(request.form['red']))
     else:
-        return render_template("index.html")
+        return render_template("index.html", cake=2)
 
 if __name__ == "__main__":
     app.run(port=5001)
